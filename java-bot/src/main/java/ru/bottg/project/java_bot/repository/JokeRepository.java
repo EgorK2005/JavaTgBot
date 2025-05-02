182954c542277ed9d49231160d4750ba801f7463
Egor Kulikov <egor_kulikov_2005@mail.ru>
2025-05-02 13:48:03 +0300

    Delete java-bot directory

--- java-bot/src/main/java/ru/bottg/project/java_bot/repository/JokeRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.bottg.project.java_bot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.bottg.project.java_bot.model.Joke;
-
-public interface JokeRepository extends JpaRepository<Joke, Long> {
-}
